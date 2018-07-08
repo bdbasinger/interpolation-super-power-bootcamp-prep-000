5725276303d863f958640c9710011fd6896c6801
Brennan B <bdbasinger777@gmail.com>
2018-07-08 02:26:03 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -7,3 +7,18 @@
 def display_rainbow(arr)
   
 end
+
+
+
+
+=begin 
+
+expect(colors).to receive(:[]).with(0).at_least(:once).and_return("red")
+expect(colors).to receive(:[]).with(1).at_least(:once).and_return("orange")
+expect(colors).to receive(:[]).with(2).at_least(:once).and_return("yellow")
+expect(colors).to receive(:[]).with(3).at_least(:once).and_return("green")
+expect(colors).to receive(:[]).with(4).at_least(:once).and_return("blue")
+expect(colors).to receive(:[]).with(5).at_least(:once).and_return("indigo")
+expect(colors).to receive(:[]).with(6).at_least(:once).and_return("violet")
+    
+    =end
